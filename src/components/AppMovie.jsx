837ce2e639707b532d4cd8f189c4159deab884bd
ErikaCetera <ceteraerika@gmail.com>
2025-01-25 15:50:48 +0100

    feat: movie page e componente card

--- src/components/AppMovie.jsx
+++ src/components/AppMovie.jsx
@@ -1,5 +1,18 @@
-const AppMovie = () => {
+const AppMovie = ({ movie }) => {
 
+    const urlBe = import.meta.env.VITE_BACKEND_URL;
+    return (
+        <>
+        <div  className="card h-100">
+            <img src={`${urlBe}${movie.image}`} className="card-img-top" alt={movie.title} />
+            <div className="card-body">
+                <h3 className="card-title">{movie.title}</h3>
+                <h5 className="card-title">{movie.director}</h5>
+                <p className="card-text">{movie.abstract}</p>
+            </div>
+            </div>
+        </>
+    )
 };
 
 export default AppMovie;
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,8 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AppMovie from "../components/AppMovie";
+
+
+//Preleva film dal server
 const MoviesPage = () => {
 
-    return(
+    const urlBe = import.meta.env.VITE_BACKEND_URL;
+    const [movies, setMovies] = useState([]);
+
+    const getMovies = () => {
+
+        axios.get(`${urlBe}/movies`).then((resp) => {
+            console.log(resp);
+            setMovies(resp.data.data);
+        })
+    };
+
+    //Mostra all'avvio della pagina la funzione per prelevare i film
+    useEffect(() => {
+        getMovies()
+    }, []);
+
+
+    return (
         <>
-        <h1>Movies Page</h1>
+            <div  className="container" >
+                <div className="row my-5">
+                    {/* //Mostra in pagina le card */}
+                    {movies.map((curMovie) => {
+                        return (
+                            <div className= "col" key={curMovie.id}>
+                            <AppMovie movie={curMovie}/>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     )
 };
